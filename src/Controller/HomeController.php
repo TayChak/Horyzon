39dce25722691e2b2e4df5275fd35e3d7a754911
Tayeb Chakroun <tayebchakroune@gmail.com>
2021-10-10 14:34:27 +0100

    add Continue reading path

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,17 +13,36 @@ class HomeController extends AbstractController
 {   
     /** 
      * @Route("/", name="homepage")
+     * @param ArticleRepository $repo
      * 
      * @return Response
      */
-    public function home(): Response
+    public function home(ArticleRepository $repo): Response
     {
         return $this->render('home.twig',
             [
-
+                'articles' => $repo->findAll(),
             ]
         );
     }
+
+
+    /** 
+     * @Route("/article/{slug}", name="article_page")
+     * @param Article $article
+     * 
+     * @return Response
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('show.twig',
+            [
+                'article' => $article
+            ]
+        );
+    }
+
+
 
 }
 ?>
